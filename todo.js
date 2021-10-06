ab223c5614ca64935ec5ddc3de9e2bb0cb0d16e5
Mkharbech salah eddine <salaheddine.mk8@gmail.com>
2021-10-06 09:17:56 +0200

    todo-list

--- todo.js
+++ todo.js
@@ -1,11 +1,62 @@
-const form = document.querySelector('.form');
-
+const form = document.querySelector('form');
+// console.log(form)
+const liste = document.querySelector('ul');
+const input  = document.querySelector('input');
+let toutesLesTaches = [];
 // function getValue (){
 // let salah = document.getElementById('message').value ;
 // alert(salah)
 // }
-const element = create
+
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    console.log(document.getElementById('message').value);
+    console.log(input.value);
+    // methode Trim enleve tout espace avant ou apres le text
+    const text = input.value.trim();
+    if(text !== ''){
+        inserUneTache(text);
+        input.value = '';
+    }  
 })
+
+function inserUneTache(text){
+    const objet = {
+        text,
+        id: Date.now()
+    }
+    afficherListe(objet)
+}
+
+function afficherListe(objet){
+    const item = document.createElement('li')
+    item.setAttribute('data-key', objet.id)
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'checkbox')
+    input.addEventListener('click', tacheFaite)
+    item.appendChild(input);
+
+    const txt = document.createElement('span')
+    txt.innerText = objet.text
+    item.appendChild(txt)
+
+    const btn = document.createElement('button')
+    // btn.setAttribute('onClick', supprimTache())
+    btn.addEventListener('click', supprimTache)
+
+    const img = document.createElement('img')
+    img.setAttribute('src', "./ressources/fermer.svg")
+    btn.appendChild(img)
+    item.appendChild(btn)
+
+    liste.appendChild(item)
+    toutesLesTaches.push(item)
+}
+
+function tacheFaite(event){
+    event.target.parentNode.classList.toggle('finDeTache')
+}
+
+function supprimTache (e){
+    e.target.parentNode.classList.add('suprripe')
+}
